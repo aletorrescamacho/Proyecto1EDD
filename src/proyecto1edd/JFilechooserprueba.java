a6ababf9da77596dfcb91754b11b6aea397b5e5e
Alessandra Torres <alessandra.torres@correo.unimet.edu.ve>
2023-10-19 13:59:05 -0400

    sol errores

--- src/proyecto1edd/JFilechooserprueba.java
+++ src/proyecto1edd/JFilechooserprueba.java
@@ -5,10 +5,28 @@
  */
 package proyecto1edd;
 
+import java.io.File;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
+
 /**
- *
- * @author Rebeca
+*Clase leer archivo (prueba)
+*@author: Alessandra Torres
+*@version: 18/10/23
  */
 public class JFilechooserprueba {
-    
+    public static void main(String[] args) {
+        
+        // Se instancia el JFilechooser
+        JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+        int returnValue = jfc.showOpenDialog(null);
+        // Verifica que el numero guardado sea el de opción aprovada, de ser así imprime su ruta
+        if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = jfc.getSelectedFile();
+                System.out.println(selectedFile.getAbsolutePath());
+        }
+
+}  
 }
