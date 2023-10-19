2af749458b6799a8580ccddf55a464e2c8eb12bd
Alessandra Torres <alessandra.torres@correo.unimet.edu.ve>
2023-10-18 22:03:21 -0400

    Creación clase vertice

--- src/proyecto1edd/Vertice.java
+++ src/proyecto1edd/Vertice.java
@@ -3,14 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Prueba;
+package proyecto1edd;
 
 /**
  *
  * @author Rebeca
  */
-public class Prueba {
-
-    
+public class Vertice {
     
 }
